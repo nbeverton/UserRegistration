c586240cea60c24c66159f74487e117ab6d8326f
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-08-31 15:23:58 -0300

    DB configuration
    Data base configuration. Using MySQL version 8.0.30. Configuration os application.properties. OK

--- src/main/java/com/nbeverton/UserRegistration/controller/TestController.java
+++ src/main/java/com/nbeverton/UserRegistration/controller/TestController.java
@@ -1,8 +1,6 @@
 package com.nbeverton.UserRegistration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +8,6 @@ public class TestController {
 
 @GetMapping(path = "/welcome")
     public String helloUser(){
-        return "User Registration";
+        return "User Registration 2.0";
     }
 }
