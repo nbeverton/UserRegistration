ffe5ff2c5ff18b0bf029b84a42bf5654f7c53c46
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-09-03 20:46:58 -0300

    Classe Agenda.
    Criação da classe agenda e configuração.

--- src/main/java/com/nbeverton/UserRegistration/controller/TestController.java
+++ src/main/java/com/nbeverton/UserRegistration/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.nbeverton.UserRegistration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class TestController {
     public String helloUser(){
         return "User Registration 2.0";
     }
+
+@RequestMapping("/Test")
+    public String test(){
+    return "Test";
+}
+
 }
---
+++ src/main/java/com/nbeverton/UserRegistration/model/Agenda.java
@@ -0,0 +1,41 @@
+package com.nbeverton.UserRegistration.model;
+
+public class Agenda {
+
+    private String name;
+    private String place;
+    private String time;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
