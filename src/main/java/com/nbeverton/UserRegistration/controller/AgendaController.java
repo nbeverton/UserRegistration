a7eacb0e45e9f1b976c2dee1eca488c7b5ca888a
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-09-08 20:57:12 -0300

    index.html
    Criação do modelo de uma página inicial usando o Bootstrap.

--- src/main/java/com/nbeverton/UserRegistration/controller/AgendaController.java
+++ src/main/java/com/nbeverton/UserRegistration/controller/AgendaController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class AgendaController {
 
     @Autowired
--- src/main/java/com/nbeverton/UserRegistration/model/repository/AgendaRepository.java
+++ src/main/java/com/nbeverton/UserRegistration/model/repository/AgendaRepository.java
@@ -3,6 +3,6 @@ package com.nbeverton.UserRegistration.model.repository;
 import com.nbeverton.UserRegistration.model.Agenda;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AgendaRepository extends CrudRepository<Agenda, String> {
+public interface AgendaRepository extends CrudRepository<Agenda, Integer> {
 
 }
