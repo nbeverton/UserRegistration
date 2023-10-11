6102d0e428f1b90206411d80073fd7ef2dbeccd5
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-10-11 17:41:58 -0300

    Create AdressRepository.java
    Criação da interface AdressRepository.
    Testado - ok

---
+++ src/main/java/com/nbeverton/UserRegistration/model/repository/AdressRepository.java
@@ -0,0 +1,8 @@
+package com.nbeverton.UserRegistration.model.repository;
+
+import com.nbeverton.UserRegistration.model.Adress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdressRepository extends JpaRepository<Adress, Integer> {
+
+}
